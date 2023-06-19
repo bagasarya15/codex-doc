4733ca36acee320a05913c807db3403f4eb9ac4c
bagasarya15 <bagasarya150800@gmail.com>
2023-06-19 15:46:17 +0700

    commit

--- my_doc/query-address.sql
+++ my_doc/query-address.sql
@@ -29,8 +29,24 @@ select * from master.city
 select * from master.address_type
 select * from master.address
 select * from users.users_address
+select * from users.users_education
+select * from users.business_entity
+select * from users.users_experiences
+truncate table users.business_entity restart identity cascade
 
-truncate table users.users_address restart identity
+
+INSERT INTO users.users_experiences(usex_entity_id, usex_title) values (2, 'Sarjana Komedi')
+select users.user_name, users_experiences.usex_title from users.users join users.users_experiences
+on users.user_entity_id = users_experiences.usex_entity_id
+
+INSERT INTO users.users_education(usdu_entity_id, usdu_school, usdu_degree, usdu_field_study, usdu_start_date, usdu_end_date, usdu_grade, usdu_activities, usdu_description) 
+VALUES (2, 'Unindra', 'Bachelor', 'Informatics Engineering', '2018-06-06', '2022-08-15', '3.42', 'Saya suka ngoding', 'saya suka ngoding tapi kalo error males');
+select users.user_name, users_education.usdu_school FROM users.users JOIN users.users_education
+ON users.user_entity_id = users_education.usdu_entity_id 
+
+select * from users.users join users.users_education on usdu_entity_id = user_entity_id
+
+truncate table users.users_education restart identity
 
 truncate table master.address restart identity cascade
 
@@ -47,5 +63,11 @@ CREATE TABLE users.users_address(
 	etad_adty_id INTEGER REFERENCES master.address_type(adty_id),
 	PRIMARY KEY(etad_addr_id)
 )
+
+SELECT * FROM master.skill_type
+insert into master.skill_type values('mysql'), ('php'), ('postgresql')
+select * from users.users_skill
+
+insert into users.users_skill(uski_entity_id, uski_skty_name) values (2, 'javascript')
 
 
