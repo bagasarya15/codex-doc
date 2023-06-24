ef6cb3c332ff0891e0f39c836befdad50c2e4665
bagasarya15 <bagasarya150800@gmail.com>
2023-06-25 00:00:24 +0700

    commit

--- my_doc/query-coba.sql
+++ my_doc/query-coba.sql
@@ -12,13 +12,11 @@ select * from users.users_roles
 select * from users.users_media
 select * from users.users_phones
 
-truncate table users.users_media restart identity
+truncate table users.business_entity restart identity cascade
 
 
 select users.user_entity_id, users.user_name, users.user_current_role, users_roles.usro_entity_id, users_roles.usro_role_id
 from users.users JOIN users.users_roles ON users.user_entity_id = users_roles.usro_entity_id
-
-truncate table users.business_entity restart identity cascade
 
 INSERT INTO users.users_experiences(usex_entity_id, usex_title) values (2, 'Sarjana Komedi')
 select users.user_name, users_experiences.usex_title from users.users join users.users_experiences
@@ -49,24 +47,30 @@ CREATE TABLE users.users_address(
 	PRIMARY KEY(etad_addr_id)
 )
 
+select * from users.users_media
 SELECT * FROM master.skill_type
 insert into master.skill_type values('mysql'), ('php'), ('postgresql')
 select * from users.users_skill
 
 insert into users.users_skill(uski_entity_id, uski_skty_name) values (2, 'javascript')
 
-call users.apply_jobs(9, 'hendri', 'prasmono', 'test-photo.jpg','codex academy university', 'Diploma', 'd3 informatika', '08127', 'mycv.pdf', 'pdf', 7 )
+call users.apply_jobs(1, 'Bagas', 'Arya', 'test-photo.jpg', '2000-08-15', 'Unindra', 'Bachelor', 
+					  'Informatika', '081285711519', 'CV', 'http://localhost:3000/user-media/bgs.pdf', 7 )
+
 CREATE OR REPLACE PROCEDURE users.apply_jobs (
     IN user_id INT,
     IN firstname VARCHAR,
     IN lastname VARCHAR,
 	IN userphoto VARCHAR,
+	IN birthdate DATE,
     IN user_school VARCHAR,
     IN user_degree VARCHAR,
     IN user_field_study VARCHAR,
 	IN user_phone_number VARCHAR,
-	IN filename VARCHAR,
-	IN filetype VARCHAR,
+	IN user_resume VARCHAR,
+	IN user_filelink VARCHAR,
+	IN user_filesize INT,
+	IN user_filetype VARCHAR,
 	IN role_id INT
 )
 LANGUAGE plpgsql
@@ -75,20 +79,15 @@ $$
 DECLARE
     update_role INT;
 BEGIN
-    UPDATE users.users_education
-    SET usdu_school = user_school,
-		usdu_degree = user_degree,
-		usdu_field_study = user_field_study
-    WHERE usdu_entity_id = user_id;
+	INSERT INTO users.users_education(usdu_entity_id, usdu_school,usdu_degree, usdu_field_study)
+	VALUES(user_id, user_school, user_degree, user_field_study);
 	
 	UPDATE users.users_phones 
 	SET uspo_number = user_phone_number 
 	WHERE uspo_entity_id = user_id;
 	
-	UPDATE users.users_media
-	SET usme_filename = filename,
-		usme_filetype = filetype 
-	WHERE usme_entity_id = user_id;
+	INSERT INTO users.users_media (usme_entity_id, usme_filename, usme_file_link, usme_filesize, usme_filetype)
+    VALUES (user_id, user_resume, user_filelink, user_filesize, user_filetype);
 	
 	UPDATE users.users_roles
 	SET usro_role_id = role_id
@@ -99,8 +98,12 @@ BEGIN
     SET user_first_name = firstname,
         user_last_name = lastname,
 		user_photo = userphoto,
+		user_birth_date = birthdate,
 		user_current_role = update_role
-    WHERE user_entity_id = user_id;
-    
+    WHERE user_entity_id = user_id; 
 END;
 $$;
+
+select * from users.users_media
+truncate table users.business_entity restart identity cascade
+
